3345596c484961e39c11ea32f57ea315fc4da73f
DDang_ <northwind3552@gmail.com>
2023-05-06 20:36:25 +0900

    추가(test1): test1 추가

---
+++ Cris.py/Assets/Scripts/test1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("테스트1 ");
+    }
+}
